4d81e3df720b520cde6591c69f3c91a2402e4992
karyakirana <adibpratama2@gmail.com>
2022-02-26 23:42:35 +0700

    add helpers

---
+++ app/Haramain/helpers.php
@@ -0,0 +1,70 @@
+<?php
+
+if (!function_exists('rupiah_format')){
+    function rupiah_format($number)
+    {
+        return number_format($number, 0, ",", ".");
+    }
+}
+
+if (!function_exists('diskon_format')){
+    function diskon_format($value, $angkaBelakangKoma)
+    {
+        return number_format($value, $angkaBelakangKoma,",", ".");
+    }
+}
+
+if (!function_exists('tanggalan_database_format')){
+    function tanggalan_database_format($tanggal, $format): string
+    {
+        return \Carbon\Carbon::createFromFormat($format, $tanggal)->format('Y-m-d');
+    }
+}
+
+if (!function_exists('tanggalan_format')){
+    function tanggalan_format($tanggal)
+    {
+        return \Carbon\Carbon::parse($tanggal)->format('d-M-Y');
+    }
+}
+
+if (!function_exists('before_string_me')){
+    function before_string_me ($char, $data)
+    {
+        return substr($data, 0, strpos($data, $char));
+    };
+}
+
+if (!function_exists('terbilang')){
+    function terbilang($angka) {
+        $angka=abs($angka);
+        $baca =array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+
+        $terbilang="";
+        if ($angka < 12){
+            $terbilang= " " . $baca[$angka];
+        }
+        else if ($angka < 20){
+            $terbilang= terbilang($angka - 10) . " belas";
+        }
+        else if ($angka < 100){
+            $terbilang= terbilang($angka / 10) . " puluh" . terbilang($angka % 10);
+        }
+        else if ($angka < 200){
+            $terbilang= " seratus" . terbilang($angka - 100);
+        }
+        else if ($angka < 1000){
+            $terbilang= terbilang($angka / 100) . " ratus" . terbilang($angka % 100);
+        }
+        else if ($angka < 2000){
+            $terbilang= " seribu" . terbilang($angka - 1000);
+        }
+        else if ($angka < 1000000){
+            $terbilang= terbilang($angka / 1000) . " ribu" . terbilang($angka % 1000);
+        }
+        else if ($angka < 1000000000){
+            $terbilang= terbilang($angka / 1000000) . " juta" . terbilang($angka % 1000000);
+        }
+        return $terbilang;
+    }
+}
