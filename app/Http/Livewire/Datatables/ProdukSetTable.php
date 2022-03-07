b94af3207a8d69496a6e1ddd472cc8b6ba89df0b
karyakirana <adibpratama2@gmail.com>
2022-03-07 12:50:42 +0700

    penjualan

--- app/Http/Livewire/Datatables/ProdukSetTable.php
+++ app/Http/Livewire/Datatables/ProdukSetTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Datatables;
 
+use App\Models\Master\Produk;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
@@ -12,13 +13,25 @@ class ProdukSetTable extends DataTableComponent
     public function columns(): array
     {
         return [
-            Column::make('Column Name'),
+            Column::make('ID', 'kode')
+                ->sortable()
+                ->searchable(),
+            Column::make('Lokal', 'kode_lokal')
+                ->searchable(),
+            Column::make('Produk', 'nama')
+                ->sortable()
+                ->searchable(),
+            Column::make('Harga')
+                ->searchable(),
+            Column::make('Cover'),
+            Column::make('Penerbit'),
+            Column::make(''),
         ];
     }
 
     public function query(): Builder
     {
-
+        return Produk::query();
     }
 
     public function rowView(): string
--- resources/views/livewire-tables/rows/produk_set_table.blade.php
+++ resources/views/livewire-tables/rows/produk_set_table.blade.php
@@ -1,5 +1,25 @@
-<x-livewire-tables::table.cell>
-{{-- Note: This is a tailwind cell --}}
-{{-- For bootstrap 4, use <x-livewire-tables::bs4.table.cell> --}}
-{{-- For bootstrap 5, use <x-livewire-tables::bs5.table.cell> --}}
-</x-livewire-tables::table.cell>
+<x-atoms.table.td width="10%" align="center">
+    {{$row->kode}}
+</x-atoms.table.td>
+<x-atoms.table.td width="10%" align="center">
+    {{$row->kode_lokal}}
+</x-atoms.table.td>
+<x-atoms.table.td width="25%">
+    {{$row->nama}}<br>
+    {{$row->kategori->nama}}
+    {{$row->kategoriHarga->nama}}
+</x-atoms.table.td>
+<x-atoms.table.td width="15%" align="end">
+    {{rupiah_format($row->harga)}}
+</x-atoms.table.td>
+<x-atoms.table.td width="10%" align="center">
+    {{$row->cover}}
+</x-atoms.table.td>
+<x-atoms.table.td width="10%">
+    {{$row->penerbit}}
+</x-atoms.table.td>
+<x-atoms.table.td width="10%" align="center">
+    <x-atoms.button.btn-icon onclick="Livewire.emit('set_produk', {{$row->id}})">
+        set
+    </x-atoms.button.btn-icon>
+</x-atoms.table.td>
