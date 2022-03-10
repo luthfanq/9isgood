4678ddb5ee9dc80b456e6066491ef49a994ab276
karyakirana <adibpratama2@gmail.com>
2022-03-10 18:37:15 +0700

    delete stock baik rusak

--- resources/views/livewire/stock/stock-keluar-baik-index.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <x-molecules.card title="Stock Keluar Baik">
-        <x-slot name="toolbar">
-            <x-atoms.button.btn-link-primary href="{{route('stock.keluar.baik.form')}}">New Data</x-atoms.button.btn-link-primary>
-        </x-slot>
-    </x-molecules.card>
-</div>
--- resources/views/livewire/stock/stock-masuk-baik-index.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <x-molecules.card title="Stock Masuk Baik">
-        <x-slot name="toolbar">
-            <x-atoms.button.btn-link-primary href="{{route('stock.masuk.baik.form')}}">New Data</x-atoms.button.btn-link-primary>
-        </x-slot>
-        <livewire:datatables.stock-masuk-table :kondisi="__('baik')"/>
-    </x-molecules.card>
-</div>
