98a823c7dee06103d3bd8156b33517e108f37720
karyakirana <adibpratama2@gmail.com>
2022-03-10 17:05:06 +0700

    commit upan stock masuk

---
+++ app/Http/Livewire/Datatables/StockMasukTable.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Livewire\Datatables;
+
+use App\Haramain\Traits\LivewireTraits\DatatablesTraits;
+use App\Models\Stock\StockMasuk;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+
+class StockMasukTable extends DataTableComponent
+{
+
+    public $kondisi;
+    protected string $pageName = 'stockMasuk';
+    protected string $tableName = 'stockMasukList';
+    use DatatablesTraits;
+
+    public function mount($kondisi = null)
+    {
+        $this->kondisi = $kondisi;
+    }
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'kode')
+                ->sortable()
+                ->searchable()
+                ->addClass('hidden md:table-cell')
+                ->selected(),
+            Column::make('Gudang', 'gudang.nama')
+                ->sortable()
+                ->searchable(),
+            Column::make('Nomor PO', 'nomor_po')
+                ->sortable()
+                ->searchable(),
+            Column::make('Supplier', 'supplier.nama')
+                ->sortable()
+                ->searchable(),
+            Column::make('Pembuat', 'users.nama')
+                ->sortable()
+                ->searchable(),
+            Column::make('Tgl Masuk', 'tgl_masuk')
+                ->sortable()
+                ->searchable(),
+            Column::make('Action', 'actions')
+                ->sortable()
+                ->searchable(),
+        ];
+    }
+
+    public function query(): Builder
+    {
+        $stockMasuk = StockMasuk::query()
+            ->with(['gudang', 'supplier', 'users'])
+            ->where('active_cash', session('ClosedCash'))
+            ->latest('kode');
+
+        if ($this->kondisi){
+            return $stockMasuk->where('kondisi', $this->kondisi);
+        }
+
+        return $stockMasuk;
+    }
+
+
+    public function edit($id)
+    {
+        return redirect()->to('stock/masuk/baik/edit/'.$id);
+    }
+
+    public function editRusak($id)
+    {
+        return redirect()->to('stock/masuk/rusak/edit/'.$id);
+    }
+
+    public function print($id)
+    {
+        return redirect()->to(''.$id);
+    }
+
+
+    public function rowView(): string
+    {
+        return 'livewire-tables.rows.stock_masuk_table';
+    }
+}
