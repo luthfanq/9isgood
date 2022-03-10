a322e14bf4530ab990f67d96a55800ab74b2cdf7
karyakirana <adibpratama2@gmail.com>
2022-03-10 17:11:24 +0700

    commit stock keluar index

---
+++ resources/views/livewire/stock/stock-keluar-rusak-index.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <x-molecules.card title="Stock Keluar Rusak">
+        <x-slot name="toolbar">
+            <x-atoms.button.btn-link-primary href="{{route('stock.keluar.rusak.form')}}">New Data</x-atoms.button.btn-link-primary>
+        </x-slot>
+    </x-molecules.card>
+</div>
