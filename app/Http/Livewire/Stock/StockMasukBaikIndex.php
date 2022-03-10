baf028d9c173e932a2fac951712c854ecf14d04b
karyakirana <adibpratama2@gmail.com>
2022-03-10 13:31:21 +0700

    commit stock 1

---
+++ app/Http/Livewire/Stock/StockMasukBaikIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Stock;
+
+use Livewire\Component;
+
+class StockMasukBaikIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.stock.stock-masuk-baik-index');
+    }
+}
