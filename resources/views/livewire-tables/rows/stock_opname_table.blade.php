25c55c50d370acba83c250d8d660fe5a16b4f881
karyakirana <adibpratama2@gmail.com>
2022-04-05 14:15:59 +0700

    dari haramain

--- resources/views/livewire-tables/rows/stock_opname_table.blade.php
+++ resources/views/livewire-tables/rows/stock_opname_table.blade.php
@@ -14,7 +14,7 @@
     {{tanggalan_format($row->tgl_input)}}
 </x-atoms.table.td>
 <x-atoms.table.td align="center" width="12%">
-        <x-atoms.button.btn-icon-link :href="url('/').'/stock/transaksi/opname/trans/'.$row->id"><i class="far fa-edit fs-4"></i></x-atoms.button.btn-icon-link>
+        <x-atoms.button.btn-icon-link :href="url('/').'/stock/transaksi/opname/trans/'.$row->jenis.'/'.$row->id"><i class="far fa-edit fs-4"></i></x-atoms.button.btn-icon-link>
         <x-atoms.button.btn-icon color="danger"><i class="bi bi-trash-fill fs-4"></i></x-atoms.button.btn-icon>
         <x-atoms.button.btn-icon><i class="fas fa-indent fs-4" onclick="Livewire.emit('showStockDetail', {{$row->id}})"></i></x-atoms.button.btn-icon>
         <x-atoms.button.btn-icon-link color="info"><i class="fas fa-print fs-4"></i></x-atoms.button.btn-icon-link>
